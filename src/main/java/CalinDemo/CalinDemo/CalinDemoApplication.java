267bd8a37bce8c349cdfe14a9dd50fe49184d1d5
CDSBeef <calincomptonsilva@outlook.com>
2021-07-03 21:22:00 +0000

    First Push

---
+++ src/main/java/CalinDemo/CalinDemo/CalinDemoApplication.java
@@ -0,0 +1,13 @@
+package CalinDemo.CalinDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalinDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CalinDemo/CalinDemo/CalinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package CalinDemo.CalinDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
